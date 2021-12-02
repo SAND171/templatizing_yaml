6ee2c848b9be3f5ff9e8af9c61669b1edce64993
Asit Parija <asit@whiteklay.com>
2021-12-02 14:43:46 +0530

    added base scripts

---
+++ start-deployment.sh
@@ -0,0 +1,42 @@
+# Should have 4 Modes of Deployment
+
+# Embedded Kafka
+# External Kafka Cluster
+# Azure Event Hub
+
+
+function deploy_embedded() {
+echo "Starting deployment Embedded"
+}
+
+
+function deploy_external() {
+
+
+}
+
+
+function deloy_azure() {
+
+
+}
+
+function set_container_registry() {
+
+
+}
+
+function install_gcp_sdk() {
+
+
+}
+
+
+
+
+#main
+
+
+
+
+
